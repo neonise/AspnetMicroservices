13cf08871c6edd9d7c797adb1cc3d8df90fd4a69
parvizwpf <30858096+parvizwpf@users.noreply.github.com>
2022-01-03 22:11:26 +0330

    ordering

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQuery : IRequest<List<OrdersVm>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrderListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public Task<bool> SendEmail(Email email)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
